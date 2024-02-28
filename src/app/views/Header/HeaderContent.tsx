7ad4b1cf7d30cb60a40fdc7c4a0b383115ae9c29
Clément Muth <clementmuth@gmail.com>
2024-02-27 19:11:40 -0800

    feat: add support button (#4)
    feat: add support button

--- src/app/views/Header/HeaderContent.tsx
+++ src/app/views/Header/HeaderContent.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import { Button } from "@nextui-org/button";
-import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/dropdown";
 import { Link } from "@nextui-org/link";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/navbar";
+import HeaderMenu from "~/app/views/Header/HeaderMenu";
 import DeleteClient from "~/core/DeleteClient";
-import NextLink from "next/link";
 
 export interface HeaderContentProps {
   clientId?: string;
@@ -32,156 +31,48 @@ const HeaderContent = ({ clientId }: HeaderContentProps) => {
       }}
     >
       <NavbarBrand>
-        <Link href="/" className="font-bold text-inherit">
-          Nextjs Stripe Example
+        <Link href="/" className="text-2xl text-inherit">
+          Nextjs Stripe
         </Link>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <Dropdown>
-          <NavbarItem>
-            <DropdownTrigger>
-              <Button
-                disableRipple
-                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                // endContent={icons.chevron}
-                radius="sm"
-                variant="light"
-              >
-                Payment methods
-              </Button>
-            </DropdownTrigger>
-          </NavbarItem>
-          <DropdownMenu
-            aria-label="Payment methods"
-            className="w-[340px]"
-            itemClasses={{
-              base: "gap-4"
-            }}
-          >
-            <DropdownItem
-              key="add payment method"
-              description="Create a new payment method."
-              href="/add-payment-methods"
-              as={NextLink}
-            >
-              Add Payment Method
-            </DropdownItem>
-            <DropdownItem
-              key="list_payment_methods"
-              description="List payment methods attached to a customer."
-              href="/get-payment-methods"
-              as={NextLink}
-            >
-              List Payment Methods
-            </DropdownItem>
-            <DropdownItem
-              key="delete_payment_methods"
-              description="Delete a payment method."
-              href="/delete-payment-methods"
-              // startContent={icons.flash}
-            >
-              Delete Payment Methods
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <Dropdown>
-          <NavbarItem>
-            <DropdownTrigger>
-              <Button
-                disableRipple
-                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                // endContent={icons.chevron}
-                radius="sm"
-                variant="light"
-              >
-                Customer
-              </Button>
-            </DropdownTrigger>
-          </NavbarItem>
-          <DropdownMenu
-            aria-label="Payment methods"
-            className="w-[340px]"
-            itemClasses={{
-              base: "gap-4"
-            }}
-          >
-            <DropdownItem
-              key="add payment method"
-              description="Create a new payment method."
-              href="/add-payment-methods"
-              as={NextLink}
-            >
-              Add Payment Method
-            </DropdownItem>
-            <DropdownItem
-              key="list_payment_methods"
-              description="List payment methods attached to a customer."
-              href="/get-payment-methods"
-              as={NextLink}
-            >
-              List Payment Methods
-            </DropdownItem>
-            <DropdownItem
-              key="delete_payment_methods"
-              description="Delete a payment method."
-              href="/delete-payment-methods"
-              // startContent={icons.flash}
-            >
-              Delete Payment Methods
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <Dropdown>
-          <NavbarItem>
-            <DropdownTrigger>
-              <Button
-                disableRipple
-                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                // endContent={icons.chevron}
-                radius="sm"
-                variant="light"
-              >
-                Invoices
-              </Button>
-            </DropdownTrigger>
-          </NavbarItem>
-          <DropdownMenu
-            aria-label="Payment methods"
-            className="w-[340px]"
-            itemClasses={{
-              base: "gap-4"
-            }}
-          >
-            <DropdownItem
-              key="add payment method"
-              description="Create a new payment method."
-              href="/add-payment-methods"
-              as={NextLink}
-            >
-              Add Payment Method
-            </DropdownItem>
-            <DropdownItem
-              key="list_payment_methods"
-              description="List payment methods attached to a customer."
-              href="/get-payment-methods"
-              as={NextLink}
-            >
-              List Payment Methods
-            </DropdownItem>
-            <DropdownItem
-              key="delete_payment_methods"
-              description="Delete a payment method."
-              href="/delete-payment-methods"
-              // startContent={icons.flash}
-            >
-              Delete Payment Methods
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+      <NavbarContent className="hidden sm:flex gap-4" justify="start">
+        <HeaderMenu />
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
           <DeleteClient clientId={clientId} />
+        </NavbarItem>
+        <NavbarItem>
+          <Button
+            color="default"
+            variant="solid"
+            href="https://www.buymeacoffee.com/clementmuth"
+            target="_blank"
+            size="md"
+            as={Link}
+            startContent={
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                height="24"
+                role="presentation"
+                viewBox="0 0 24 24"
+                width="24"
+                className="text-danger group-data-[hover=true]:animate-heartbeat"
+                tabIndex={-1}
+              >
+                <path
+                  d="M12.62 20.81c-.34.12-.9.12-1.24 0C8.48 19.82 2 15.69 2 8.69 2 5.6 4.49 3.1 7.56 3.1c1.82 0 3.43.88 4.44 2.24a5.53 5.53 0 0 1 4.44-2.24C19.51 3.1 22 5.6 22 8.69c0 7-6.48 11.13-9.38 12.12Z"
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                />
+              </svg>
+            }
+          >
+            Support
+          </Button>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
---
+++ src/app/views/Header/HeaderMenu.tsx
@@ -0,0 +1,153 @@
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, NavbarItem } from "@nextui-org/react";
+import NextLink from "next/link";
+import ChevronDown from "~/components/Icons/chevronDown";
+
+const HeaderMenu = () => {
+  return (
+    <NavbarItem>
+      <Dropdown>
+        <NavbarItem>
+          <DropdownTrigger>
+            <Button
+              disableRipple
+              className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+              endContent={<ChevronDown fill="currentColor" size={16} />}
+              radius="sm"
+              variant="light"
+            >
+              Payment methods
+            </Button>
+          </DropdownTrigger>
+        </NavbarItem>
+        <DropdownMenu
+          aria-label="Payment methods"
+          className="w-[340px]"
+          itemClasses={{
+            base: "gap-4"
+          }}
+        >
+          <DropdownItem
+            key="add payment method"
+            description="Create a new payment method."
+            href="/add-payment-methods"
+            as={NextLink}
+          >
+            Add Payment Method
+          </DropdownItem>
+          <DropdownItem
+            key="list_payment_methods"
+            description="List payment methods attached to a customer."
+            href="/get-payment-methods"
+            as={NextLink}
+          >
+            List Payment Methods
+          </DropdownItem>
+          <DropdownItem
+            key="delete_payment_methods"
+            description="Delete a payment method."
+            href="/delete-payment-methods"
+            // startContent={icons.flash}
+          >
+            Delete Payment Methods
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <Dropdown>
+        <NavbarItem>
+          <DropdownTrigger>
+            <Button
+              disableRipple
+              className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+              // endContent={icons.chevron}
+              radius="sm"
+              variant="light"
+            >
+              Customer
+            </Button>
+          </DropdownTrigger>
+        </NavbarItem>
+        <DropdownMenu
+          aria-label="Payment methods"
+          className="w-[340px]"
+          itemClasses={{
+            base: "gap-4"
+          }}
+        >
+          <DropdownItem
+            key="add payment method"
+            description="Create a new payment method."
+            href="/add-payment-methods"
+            as={NextLink}
+          >
+            Add Payment Method
+          </DropdownItem>
+          <DropdownItem
+            key="list_payment_methods"
+            description="List payment methods attached to a customer."
+            href="/get-payment-methods"
+            as={NextLink}
+          >
+            List Payment Methods
+          </DropdownItem>
+          <DropdownItem
+            key="delete_payment_methods"
+            description="Delete a payment method."
+            href="/delete-payment-methods"
+            // startContent={icons.flash}
+          >
+            Delete Payment Methods
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <Dropdown>
+        <NavbarItem>
+          <DropdownTrigger>
+            <Button
+              disableRipple
+              className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+              // endContent={icons.chevron}
+              radius="sm"
+              variant="light"
+            >
+              Invoices
+            </Button>
+          </DropdownTrigger>
+        </NavbarItem>
+        <DropdownMenu
+          aria-label="Payment methods"
+          className="w-[340px]"
+          itemClasses={{
+            base: "gap-4"
+          }}
+        >
+          <DropdownItem
+            key="add payment method"
+            description="Create a new payment method."
+            href="/add-payment-methods"
+            as={NextLink}
+          >
+            Add Payment Method
+          </DropdownItem>
+          <DropdownItem
+            key="list_payment_methods"
+            description="List payment methods attached to a customer."
+            href="/get-payment-methods"
+            as={NextLink}
+          >
+            List Payment Methods
+          </DropdownItem>
+          <DropdownItem
+            key="delete_payment_methods"
+            description="Delete a payment method."
+            href="/delete-payment-methods"
+            // startContent={icons.flash}
+          >
+            Delete Payment Methods
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </NavbarItem>
+  );
+};
+
+export default HeaderMenu;
